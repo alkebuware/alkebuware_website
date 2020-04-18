1f8fd816e548ad21b6e8276c0eadd8ded8a2e723
Tariq <tariq@alkebuware.com>
2020-04-17 20:53:36 -0400

    :zap: Initialize Repo :zap:

---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "alkebuware_website";
+const unsigned int kFlutterWindowWidth = 800;
+const unsigned int kFlutterWindowHeight = 600;
